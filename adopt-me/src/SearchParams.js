2720353d5d8b50dd4084d2ccc8565e9e75de1636
GregorSondermeier <gregor.sondermeier.github@gmail.com>
2019-06-10 16:12:46 +0200

    breed dropdown

--- adopt-me/src/SearchParams.js
+++ adopt-me/src/SearchParams.js
@@ -9,6 +9,8 @@ const SearchParams = () => {
   // btw: this is ES6 destructuring; useState always return an array
   const [location, setLocation] = useState("Seattle, WA");
   const [animal, setAnimal] = useState("dog");
+  const [breed, setBreed] = useState("");
+  const [breeds, setBreeds] = useState([]);
 
   return (
     <div className="search-params">
@@ -37,6 +39,24 @@ const SearchParams = () => {
             {ANIMALS.map(animal => (
               <option key={animal} value={animal}>
                 {animal}
+              </option>
+            ))}
+          </select>
+        </label>
+        <label htmlFor="breed">
+          Breed
+          <select
+            name="breed"
+            id="breed"
+            value={breed}
+            onChange={e => setBreed(e.target.value)}
+            onBlur={e => setBreed(e.target.value)}
+            disabled={!breeds.length}
+          >
+            <option>All</option>
+            {breeds.map(breedString => (
+              <option key={breedString} value={breedString}>
+                {breedString}
               </option>
             ))}
           </select>
