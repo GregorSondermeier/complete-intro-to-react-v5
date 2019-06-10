12e7273645def55ad4a7bf6c07b413fed991ed8f
GregorSondermeier <gregor.sondermeier.github@gmail.com>
2019-06-10 12:31:23 +0200

    continuation indent

--- adopt-me/src/App.js
+++ adopt-me/src/App.js
@@ -1,10 +1,9 @@
-const Pet = ({ name, animal, breed }) => {
+const Pet = ({name, animal, breed}) => {
   return React.createElement('div', {}, [
-      React.createElement('h1', {}, name),
-      React.createElement('h2', {}, animal),
-      React.createElement('h2', {}, breed),
-    ]
-  )
+    React.createElement('h1', {}, name),
+    React.createElement('h2', {}, animal),
+    React.createElement('h2', {}, breed),
+  ]);
 };
 
 // your code is going to go here, for now
@@ -13,22 +12,22 @@ const App = () => {
     'div',
     { id: 'something-important' },
     [
-        React.createElement('h1', {}, 'Adopt Me!'),
-        React.createElement(Pet, {
-          name: 'Luna',
-          animal: 'Dog',
-          breed: 'Havanese'
-        }),
-        React.createElement(Pet, {
-          name: 'Pepper',
-          animal: 'Bird',
-          breed: 'Cockatiel'
-        }),
-        React.createElement(Pet, {
-          name: 'Doink',
-          animal: 'Cat',
-          breed: 'Mixed'
-        })
+      React.createElement('h1', {}, 'Adopt Me!'),
+      React.createElement(Pet, {
+        name: 'Luna',
+        animal: 'Dog',
+        breed: 'Havanese'
+      }),
+      React.createElement(Pet, {
+        name: 'Pepper',
+        animal: 'Bird',
+        breed: 'Cockatiel'
+      }),
+      React.createElement(Pet, {
+        name: 'Doink',
+        animal: 'Cat',
+        breed: 'Mixed'
+      })
     ]
   )
 };
