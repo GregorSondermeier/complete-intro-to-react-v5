03c080d097b64a8d9fd38f86ffe146620ba3fc6e
GregorSondermeier <gregor.sondermeier.github@gmail.com>
2019-06-11 20:35:24 +0200

    convert Details.js to a class component

--- adopt-me/src/Details.js
+++ adopt-me/src/Details.js
@@ -1,11 +1,42 @@
 import React from "react";
+import pet from "@frontendmasters/pet";
 
-const Details = props => {
-  return (
-    <pre>
-      <code>{JSON.stringify(props, null, 2)}</code>
-    </pre>
-  );
-};
+// on class components, there is one hard requirement: to implement the render() method.
+// also we can't use hooks (i.e. useState()) with class components (yet?),
+// so we need to implement the lifecycle methods instead
+class Details extends React.Component {
+  constructor(props) {
+    super(props);
+
+    // because this is the first time the state is set, it is set directly instead of using this.setState()
+    this.state = {
+      loading: true
+    };
+  }
+
+  componentDidMount() {
+    // the props are immutable, because the child receives them from the parent
+    pet.animal(this.props.id).then(({ animal }) => {
+      // shallow merge: setState() basically does Object.assign (oldState, newState)
+      this.setState({
+        name: animal.name,
+        animal: animal.type,
+        location: `${pet.contact.address.city}, ${pet.contact.address.state}`,
+        description: animal.description,
+        media: animal.photos,
+        breed: animal.breeds.primary,
+        loading: false
+      });
+    }, console.error);
+  }
+
+  render() {
+    return (
+      <pre>
+        <code>{JSON.stringify(this.props, null, 2)}</code>
+      </pre>
+    );
+  }
+}
 
 export default Details;
